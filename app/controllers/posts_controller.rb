4edfa0b1a7ff450688e764e98df79ede0841eb71
Chris Gilbert <chrisgilbert4@icloud.com>
2019-03-16 09:37:15 +0000

    created simple has_and_belongs_to_many rel

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def index
-    @user = User.first
-    @posts = Post.joins(comments: :user).where(comments: { user_id: @user.id }).distinct
+    # @user = User.first
+    # @posts = Post.joins(comments: :user).where(comments: { user_id: @user.id }).distinct
+    @posts = Post.all
   end
 end
